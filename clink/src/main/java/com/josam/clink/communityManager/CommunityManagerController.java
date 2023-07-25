fe7a42cd01e79f2802c199c5e53f28c3732a1824
soieu <soieu9898@gmail.com>
2023-07-25 09:55:31 +0900

    temp

--- clink/src/main/java/com/josam/clink/communityManager/CommunityManagerController.java
+++ clink/src/main/java/com/josam/clink/communityManager/CommunityManagerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,5 +28,20 @@ public class CommunityManagerController {
 	@ResponseBody
 	public List<CommentVO> getComment(@RequestParam int board_no){
 		return communityManagerService.getComment(board_no);
+	}
+	
+	/**
+	 * 댓글달기
+	 * parent_id가 안들어온 경우 -> 일반댓글
+	 * parent_id가 들어온 경우 -> 대댓글
+	 * @param vo
+	 */
+	@PostMapping("/post/comment/insert")
+	@ResponseBody
+	public void insertComment(@RequestBody CommentVO cvo) {
+		if(cvo.getParent_id().isEmpty()) { // 일반댓글
+			
+		}
+		communityManagerService.insertComment(cvo);
 	}
 }

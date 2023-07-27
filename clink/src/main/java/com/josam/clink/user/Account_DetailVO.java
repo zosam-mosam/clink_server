aa9834123e14e513e27387efac3cd872cc5c5ce5
Nohkki <jin0201948@gmail.com>
2023-07-27 11:08:16 +0900

    add: 개인정보 수정

--- clink/src/main/java/com/josam/clink/user/Account_DetailVO.java
+++ clink/src/main/java/com/josam/clink/user/Account_DetailVO.java
@@ -5,13 +5,13 @@ import lombok.Data;
 @Data
 public class Account_DetailVO {
 	String account_no;
-	char bank_code;
+	String bank_code;
 	String user_no;
 	float account_balance;
-	char account_code;
-	char register_datetime;
+	String account_code;
+	String register_datetime;
 	String register_id;
-	char update_datetime;
+	String update_datetime;
 	String update_id;
 
 // react에서 보면서 맞추려고 남겨둠,,,.,,.,.,.,.,.,

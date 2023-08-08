e328e2d263e59574bdb487572bb0ef2e415767b4
HyeyoungCho <HyeyoungCho97@gmail.com>
2023-08-08 16:10:57 +0900

    merge Compelete

--- clink/src/main/java/com/josam/clink/challenge/ChallengeController.java
+++ clink/src/main/java/com/josam/clink/challenge/ChallengeController.java
@@ -95,7 +95,8 @@ public class ChallengeController {
 	@PostMapping("/register")
 	@ResponseBody
 	public void registerChallenge (@RequestBody ChallengeVO cvo) {
-		challengeService.registerChallenge(cvo);
+		System.out.println(cvo);
+		//		challengeService.registerChallenge(cvo);
 	}
 	
 	

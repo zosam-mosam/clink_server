7bfa0e7dbdd379c735b1d2ca0814de7e72cc2fbb
배동우 <ehddn5410@naver.com>
2023-08-11 10:56:15 +0900

    delete properties

--- clink/src/main/java/com/josam/clink/communityPost/CommunityPostController.java
+++ clink/src/main/java/com/josam/clink/communityPost/CommunityPostController.java
@@ -1,6 +1,15 @@
 package com.josam.clink.communityPost;
 
+import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import org.jsoup.nodes.Document;
+import org.apache.ibatis.annotations.Param;
+
+import org.apache.ibatis.annotations.Param;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,43 +18,49 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.josam.clink.financeInfo.NewsVO;
+
+
 
 @Controller
 public class CommunityPostController {
 	
 	@Autowired
-	CommunityPostService commPService;
+	private CommunityPostService commPService;
 	
 	@GetMapping("/community/posts")
 	@ResponseBody
-	public List<CommunityPostVO> getPosts(@RequestParam String category_no, @RequestParam String filter, @RequestParam String hashtag ){
-//		System.out.println(category_no);
+	public List<CommunityPostVO> getPosts(@RequestParam Map<String, Object> parameters){
+		String category_no = (String) parameters.get("category_no");
+	    String hashtag = (String) parameters.get("hashtag");
+	    String filter = (String) parameters.get("filter");
+	    
+		List<CommunityPostVO> data = new ArrayList<>();
+		
 		// 1. category_no = 0 => 베스트 게시판
-		System.out.println(hashtag);
 		if("0".equals(category_no)) {
-			return commPService.getBestPosts();
+			data = commPService.getBestPosts();
+			return data;
 		}
 		// 2. category_no != 0 && filter = 1 (최신순)
 		if("1".equals(filter)){
-			return commPService.getPostsbyRecent(category_no,hashtag);
+			data = commPService.getPostsbyRecent(parameters);
 		}
 		// 3. category_no != 0 && filter = 2 (인기순)
 		if("2".equals(filter)){
-			return commPService.getPostsbyLike(category_no,hashtag);
+			data = commPService.getPostsbyLike(category_no,hashtag);
 		}
-		return null;
+		return data;
 		
 	}
 	
 	@GetMapping("/community/hot-posts")
 	@ResponseBody
-	public HotPostList getHotPost(){//최근 인기 게시물 가져오기
-		HotPostList hpl = new HotPostList();
-		hpl.setHotPost(commPService.HotPost());
-		hpl.setHotFreePost(commPService.HotFreePost());
-		hpl.setHotInfoPost(commPService.HotInfoPost());
-		hpl.setHotAnnPost(commPService.HotAnnPost());
-		return hpl;
+	public List<Object> getHotPost() throws IOException{
+		List<Object> communityInfo =new ArrayList<>();
+		communityInfo.add(commPService.getHotPostList()); //최근 인기 게시물 리스
+		communityInfo.add(commPService.getFinNewsData());
+		return communityInfo;
 	}
 	
 	@GetMapping("/community/hot-hashtag")

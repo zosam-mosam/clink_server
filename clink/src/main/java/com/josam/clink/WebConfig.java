8d1051cc7fc40fff50ecdbc98547ff63c71aa620
HyeyoungCho <HyeyoungCho97@gmail.com>
2023-08-16 09:55:08 +0900

    update 이미지경로 - add WebConfig

--- clink/src/main/java/com/josam/clink/WebConfig.java
+++ clink/src/main/java/com/josam/clink/WebConfig.java
@@ -2,17 +2,26 @@ package com.josam.clink;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig  implements WebMvcConfigurer{
 	
 	 @Override
-	    public void addCorsMappings(CorsRegistry registry) {
+	   public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	        	.allowedOrigins("http://localhost:3000") // 허용할 출처
+	        	.allowedOrigins("http://43.200.204.75:8000") // 허용할 출처
 	            .allowedMethods("GET", "POST") // 허용할 HTTP method
 	            .allowCredentials(true) // 쿠키 인증 요청 허용
 	            .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
 	    }
+	 
+	 @Override
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		 registry.addResourceHandler("/images/**")
+		 		.addResourceLocations("file:///home/ubuntu/property/img/");
+		 
+	 }
+	 
 }

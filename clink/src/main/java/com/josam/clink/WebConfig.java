e8a7645627955cb7d6208631c697c68da014f973
HyeyoungCho <HyeyoungCho97@gmail.com>
2023-08-16 10:12:50 +0900

    update 이미지경로 - update CustomServletConfig

--- clink/src/main/java/com/josam/clink/WebConfig.java
+++ clink/src/main/java/com/josam/clink/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig  implements WebMvcConfigurer{
 	 @Override
 	   public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	        	.allowedOrigins("http://43.200.204.75:8000") // 허용할 출처
+	        	.allowedOrigins("http://43.200.204.75:80") // 허용할 출처
 	            .allowedMethods("GET", "POST") // 허용할 HTTP method
 	            .allowCredentials(true) // 쿠키 인증 요청 허용
 	            .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
